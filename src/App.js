c020db013ec32513768f283ef8f1a10a4489f8f9
lakhanprajapat1 <lakhan10209@gmail.com>
2025-01-10 19:43:32 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AgeCalculator from './components/AgeCalculator';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AgeCalculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AgeCalculator.css';
+
+const AgeCalculator = () => {
+  return (
+    <div className='container'>
+        
+    </div>
+  )
+}
+
+export default AgeCalculator;
